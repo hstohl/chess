047fb9b89d03cdd48d506b12adbd337eb91d507c
Hudson Stohl <hrstohl02@gmail.com>
2024-09-19 18:55:59 -0600

    Moved Bishop Code to own class and fixed interface

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -80,88 +80,11 @@ public class ChessPiece {
     //return PieceMovesCalculator.pieceMoves(board, myPosition);
     ArrayList<ChessMove> moves = new ArrayList<>();
     if (type == ChessPiece.PieceType.BISHOP) {
-      boolean upLeft = true;
-      boolean downLeft = true;
-      boolean upRight = true;
-      boolean downRight = true;
-      int i = 1;
-      while (upLeft || downLeft || upRight || downRight) {
-        ChessPosition newPos;
-        ChessMove move;
-        if (upRight && myPosition.getRow() + i <= 8 && myPosition.getColumn() + i <= 8) {
-          newPos = new ChessPosition(myPosition.getRow() + i, myPosition.getColumn() + i);
-          if (ChessBoard.getPiece(newPos) != null && ChessBoard.getPiece(newPos).getTeamColor() == pieceColor) {
-            upRight = false;
-            continue;
-          } else if (ChessBoard.getPiece(newPos) != null && ChessBoard.getPiece(newPos).getTeamColor() != pieceColor) {
-            move = new ChessMove(myPosition, newPos, null);
-            moves.add(move);
-            upRight = false;
-            continue;
-          }
-          move = new ChessMove(myPosition, newPos, null);
-          moves.add(move);
-        } else {
-          //System.out.println("upRightFalse: {" + (myPosition.getRow() + i) + "," + (myPosition.getColumn() + i) + "}");
-          upRight = false;
-        }
+      PieceMovesCalculator forBishop = new BishopMovesCalculator();
+      moves = forBishop.pieceMoves(board, myPosition);
 
-        if (downRight && myPosition.getRow() - i > 0 && myPosition.getColumn() + i <= 8) {
-          newPos = new ChessPosition(myPosition.getRow() - i, myPosition.getColumn() + i);
-          if (ChessBoard.getPiece(newPos) != null && ChessBoard.getPiece(newPos).getTeamColor() == pieceColor) {
-            downRight = false;
-            continue;
-          } else if (ChessBoard.getPiece(newPos) != null && ChessBoard.getPiece(newPos).getTeamColor() != pieceColor) {
-            move = new ChessMove(myPosition, newPos, null);
-            moves.add(move);
-            downRight = false;
-            continue;
-          }
-          move = new ChessMove(myPosition, newPos, null);
-          moves.add(move);
-        } else {
-          //System.out.println("downLeftFalse: {" + (myPosition.getRow() - i) + "," + (myPosition.getColumn() + i) + "}");
-          downRight = false;
-        }
-
-        if (upLeft && myPosition.getRow() + i <= 8 && myPosition.getColumn() - i > 0) {
-          newPos = new ChessPosition(myPosition.getRow() + i, myPosition.getColumn() - i);
-          if (ChessBoard.getPiece(newPos) != null && ChessBoard.getPiece(newPos).getTeamColor() == pieceColor) {
-            upLeft = false;
-            continue;
-          } else if (ChessBoard.getPiece(newPos) != null && ChessBoard.getPiece(newPos).getTeamColor() != pieceColor) {
-            move = new ChessMove(myPosition, newPos, null);
-            moves.add(move);
-            upLeft = false;
-            continue;
-          }
-          move = new ChessMove(myPosition, newPos, null);
-          moves.add(move);
-        } else {
-          //System.out.println("upLeftFalse: {" + (myPosition.getRow() + i) + "," + (myPosition.getColumn() - i) + "}");
-          upLeft = false;
-        }
-
-        if (downLeft && myPosition.getRow() - i > 0 && myPosition.getColumn() - i > 0) {
-          newPos = new ChessPosition(myPosition.getRow() - i, myPosition.getColumn() - i);
-          if (ChessBoard.getPiece(newPos) != null && ChessBoard.getPiece(newPos).getTeamColor() == pieceColor) {
-            downLeft = false;
-            continue;
-          } else if (ChessBoard.getPiece(newPos) != null && ChessBoard.getPiece(newPos).getTeamColor() != pieceColor) {
-            move = new ChessMove(myPosition, newPos, null);
-            moves.add(move);
-            downLeft = false;
-            continue;
-          }
-          move = new ChessMove(myPosition, newPos, null);
-          moves.add(move);
-        } else {
-          //System.out.println("downLeftFalse: {" + (myPosition.getRow() - i) + "," + (myPosition.getColumn() - i) + "}");
-          downLeft = false;
-        }
-        ++i;
-      }
     }
+
     return moves;
   }
 

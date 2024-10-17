d2ea40027518a0c83abb6b2eda2b50c409466d15
Hudson Stohl <hrstohl02@gmail.com>
2024-10-17 12:06:00 -0600

    Register makes the full loop. No authTokens

--- server/src/main/java/dataaccess/MemoryDataAccess.java
+++ server/src/main/java/dataaccess/MemoryDataAccess.java
@@ -2,13 +2,23 @@ package dataaccess;
 
 import model.UserData;
 
+import java.util.ArrayList;
+import java.util.Objects;
+
 public class MemoryDataAccess implements DataAccess {
+  private ArrayList<UserData> myUserData = new ArrayList<>();
+
   public UserData getUser(String username) {
-    UserData newUser = new UserData(username, "password", "email");
+    UserData newUser = null;
+    for (UserData data : myUserData) {
+      if (Objects.equals(data.username(), username)) {
+        newUser = data;
+      }
+    }
     return newUser;
   }
 
   public void addUser(UserData newUser) {
-
+    myUserData.add(newUser);
   }
 }
--- server/src/test/java/dataaccess/DataAccessTests.java
+++ server/src/test/java/dataaccess/DataAccessTests.java
@@ -1,10 +1,16 @@
 package dataaccess;
 
+import model.UserData;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class DataAccessTests {
-  @Test
-  public void getUser() {
+  private final DataAccess access = new MemoryDataAccess();
 
+  @Test
+  public void addUser() {
+    UserData user = new UserData("name", "password", "email");
+    access.addUser(user);
+    Assertions.assertEquals(user, access.getUser("name"));
   }
 }

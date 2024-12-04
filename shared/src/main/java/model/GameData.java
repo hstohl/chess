a91b0306699744e45db94579927582bb2cea76ac
Hudson Stohl <hrstohl02@gmail.com>
2024-12-04 12:49:52 -0700

    Altered GameData to support gameOver

--- shared/src/main/java/model/GameData.java
+++ shared/src/main/java/model/GameData.java
@@ -2,5 +2,9 @@ package model;
 
 import chess.ChessGame;
 
-public record GameData(int gameID, String whiteUsername, String blackUsername, String gameName, ChessGame game) {
+public record GameData(int gameID, String whiteUsername, String blackUsername, String gameName, ChessGame game,
+                       boolean isOver) {
+  public GameData(int gameID, String whiteUsername, String blackUsername, String gameName, ChessGame game) {
+    this(gameID, whiteUsername, blackUsername, gameName, game, false); // Default to 'not over'
+  }
 }
